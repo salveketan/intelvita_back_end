5123e02ab87f2e89db1c185e5ac4feb5ef727491
salveketan <salveketan6@gmail.com>
2022-11-19 20:53:00 +0530

    packjson update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use("", require("./controller/userController"))
 app.use("", require("./controller/productController"))
 app.use("", require('./controller/cartContrller'))
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 app.set("port", PORT)
 
 app.listen(PORT, () => {
